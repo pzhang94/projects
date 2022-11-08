c9b107ab2fd06eb34c24202516e79122e8828f1e
Philip Zhang <philipzhang321@gmail.com>
2022-11-08 12:02:27 -0500

    implementing hecto text editor

--- Rust/hecto/src/main.rs
+++ Rust/hecto/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+mod document;
+mod editor;
+mod row;
+mod terminal;
+use editor::Editor;
+pub use document::Document;
+pub use editor::Position;
+pub use editor::SearchDirection;
+pub use row::Row;
+pub use terminal::Terminal;
+
+fn main() 
+{
+    Editor::default().run();
 }
